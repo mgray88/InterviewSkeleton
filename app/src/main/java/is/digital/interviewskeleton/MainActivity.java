0fc08cc05f3c59a12726dadacb8be408422507be
Mike Gray <mgray88@gmail.com>
2020-06-22 15:27:11 -0400

    Add some more stubbed dependencies

--- app/src/main/java/is/digital/interviewskeleton/MainActivity.java
+++ app/src/main/java/is/digital/interviewskeleton/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.todo_list);
+        setContentView(R.layout.main_activity);
         getSupportActionBar().setTitle("TODO!");
     }
 
